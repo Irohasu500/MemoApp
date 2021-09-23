61455798606e92f716185dd0abfd94f423f36462
hasu355 <66102907+Irohasu500@users.noreply.github.com>
2021-09-24 06:16:35 +0900

    Make components touchable

--- App.jsx
+++ App.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import {} from 'react-native';
 
 // import MemoEditScreen from './src/screens/MemoEditScreen';
-// import MemoListScreen from './src/screens/MemoListScreen';
+import MemoListScreen from './src/screens/MemoListScreen';
 // import MemoDetailScreen from './src/screens/MemoDetailScreen';
 // import MemoCreateScreen from './src/screens/MemoCreateScreen';
 // import LoginScreens from './src/screens/LoginScreen';
-import SignUpScreen from './src/screens/SignUpScreen';
+// import SignUpScreen from './src/screens/SignUpScreen';
 
 // 変数が定義される前に使用されている。
 export default function App() {
-  return <SignUpScreen />;
+  return <MemoListScreen />;
 }
