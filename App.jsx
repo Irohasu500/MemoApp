457100a1b7657042871d420f764f92a54cb95201
hasu355 <66102907+Irohasu500@users.noreply.github.com>
2021-09-24 00:44:28 +0900

    Install @expo/vector-icons

--- App.jsx
+++ App.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import {} from 'react-native';
 
 import MemoListScreen from './src/screens/MemoListScreen';
-import MemoDetailScreen from './src/screens/MemoDetailScreen';
+// import MemoDetailScreen from './src/screens/MemoDetailScreen';
 
 // 変数が定義される前に使用されている。
 export default function App() {
-  return <MemoDetailScreen />;
+  return <MemoListScreen />;
 }
