af7e595b190cb9ad6572f0855d712ec30928d730
hasu355 <66102907+Irohasu500@users.noreply.github.com>
2021-09-24 03:19:32 +0900

    Create MemoEditScreen and MemoCreateScreen

--- App.jsx
+++ App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {} from 'react-native';
 
-import MemoListScreen from './src/screens/MemoListScreen';
-import MemoDetailScreen from './src/screens/MemoDetailScreen';
+// import MemoEditScreen from './src/screens/MemoEditScreen';
+// import MemoListScreen from './src/screens/MemoListScreen';
+// import MemoDetailScreen from './src/screens/MemoDetailScreen';
+import MemoCreateScreen from './src/screens/MemoCreateScreen';
 
 // 変数が定義される前に使用されている。
 export default function App() {
-  return <MemoListScreen />;
+  return <MemoCreateScreen />;
 }
