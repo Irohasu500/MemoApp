0cdd4faa6dbcfb4e67c1f885bb61b8f864c11471
hasu355 <66102907+Irohasu500@users.noreply.github.com>
2021-09-24 05:04:23 +0900

    Create LogInScreen and SignUpScreen

--- App.jsx
+++ App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import {} from 'react-native';
 
-import MemoEditScreen from './src/screens/MemoEditScreen';
+// import MemoEditScreen from './src/screens/MemoEditScreen';
 // import MemoListScreen from './src/screens/MemoListScreen';
 // import MemoDetailScreen from './src/screens/MemoDetailScreen';
 // import MemoCreateScreen from './src/screens/MemoCreateScreen';
+// import LoginScreens from './src/screens/LoginScreen';
+import SignUpScreen from './src/screens/SignUpScreen';
 
 // 変数が定義される前に使用されている。
 export default function App() {
-  return <MemoEditScreen />;
+  return <SignUpScreen />;
 }
