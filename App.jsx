022d6261190c75cb35ed41fce4cb4e8cb54ae1cd
hasu355 <66102907+Irohasu500@users.noreply.github.com>
2021-09-24 02:03:43 +0900

    Create Custiom icon

--- App.jsx
+++ App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {} from 'react-native';
 
 import MemoListScreen from './src/screens/MemoListScreen';
-// import MemoDetailScreen from './src/screens/MemoDetailScreen';
+import MemoDetailScreen from './src/screens/MemoDetailScreen';
 
 // 変数が定義される前に使用されている。
 export default function App() {
