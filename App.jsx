df273157be4ebcf74696ef97d8e7fc2e4dff93d5
hasu355 <66102907+Irohasu500@users.noreply.github.com>
2021-09-27 23:19:19 +0900

    Connect screens with react-navigation

--- App.jsx
+++ App.jsx
@@ -1,14 +1,54 @@
 import React from 'react';
-import {} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+// import {} from 'react-native';
+import { createStackNavigator, CardStyleInterpolators } from '@react-navigation/stack';
+// import { createStackNavigator } from "react-navigation-stack";
 
-// import MemoEditScreen from './src/screens/MemoEditScreen';
+import MemoEditScreen from './src/screens/MemoEditScreen';
 import MemoListScreen from './src/screens/MemoListScreen';
-// import MemoDetailScreen from './src/screens/MemoDetailScreen';
-// import MemoCreateScreen from './src/screens/MemoCreateScreen';
-// import LoginScreens from './src/screens/LoginScreen';
-// import SignUpScreen from './src/screens/SignUpScreen';
+import MemoDetailScreen from './src/screens/MemoDetailScreen';
+import MemoCreateScreen from './src/screens/MemoCreateScreen';
+import LoginScreens from './src/screens/LoginScreen';
+import SignUpScreen from './src/screens/SignUpScreen';
+
+const Stack = createStackNavigator();
 
 // 変数が定義される前に使用されている。
 export default function App() {
-  return <MemoListScreen />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="SignUp"
+        screenOptions={{
+          headerStyle: { backgroundColor: '#467FD3' },
+          headerTitleStyle: { color: '#ffffff' },
+          headerTitle: 'Memo App',
+          headerTintColor: '#ffffff',
+          headerBackTitle: 'Back',
+          cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+          gestureEnabled: true,
+          gestureDirection: 'horizontal',
+        }}
+      >
+        <Stack.Screen name="MemoList" component={MemoListScreen} />
+        <Stack.Screen name="MemoDetail" component={MemoDetailScreen} />
+        <Stack.Screen name="MemoEdit" component={MemoEditScreen} />
+        <Stack.Screen name="MemoCreate" component={MemoCreateScreen} />
+        <Stack.Screen
+          name="LogIn"
+          component={LoginScreens}
+          options={{
+            cardStyleInterpolator: CardStyleInterpolators.forFadeFromBottomAndroid,
+          }}
+        />
+        <Stack.Screen
+          name="SignUp"
+          component={SignUpScreen}
+          options={{
+            cardStyleInterpolator: CardStyleInterpolators.forFadeFromBottomAndroid,
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
